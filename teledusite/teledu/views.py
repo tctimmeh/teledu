694ae96742bfa55475499640b34af8a9ea5a170b
Tim Court <tctimmeh@gmail.com>
2012-09-29 23:34:52 -0600

    changed char sheet view to demonstrate how custom char sheets could be loaded from external data source

--- teledusite/teledu/views.py
+++ teledusite/teledu/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.template import RequestContext, Template
 from models import Character
 
 def hello(request):
@@ -7,4 +8,31 @@ def hello(request):
 
 def charSheet(request, charId):
   character = get_object_or_404(Character, id = charId)
-  return render(request, 'characterSheet.html', {'character': character})
+
+  templateHeader =  '''{% extends "characterSheet.html" %}
+{% load character %}
+{% block characterSheet %}
+'''
+
+  templateData = '''<p>
+  <b>Name</b>: {{ character.name }}
+</p>
+{% for attribute in character.attributes.all %}
+  <p>
+    <b>{{ attribute.name }}</b>: {{ character|charAttr:attribute }}
+  </p>
+{% endfor %}'''
+
+  templateFooter = '''
+{% endblock %}
+'''
+  templateString = '%s%s%s' % (templateHeader, templateData, templateFooter)
+  context = RequestContext(request)
+  context.update({
+    'character': character,
+  })
+  template = Template(templateString)
+  output = template.render(context)
+
+  return HttpResponse(output)
+  render()
