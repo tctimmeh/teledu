bb5cee5e6fbc8053de055c6e123ef327b326e6e5
Tim Court <tctimmeh@gmail.com>
2012-10-01 19:32:33 -0600

    removed the prototype charAttr template filter in favour of new char_attr tag

--- teledusite/teledu/templatetags/character.py
+++ teledusite/teledu/templatetags/character.py
@@ -1,7 +1,18 @@
 from django import template
+from ..models import CharacterAttributeDefinition
 
 register = template.Library()
 
-@register.filter(name = 'charAttr')
-def charAttr(character, attribute):
-  return character.getAttribute(attribute)
+@register.simple_tag(takes_context = True)
+def char_attr(context, attribute):
+  character = context['character']
+
+  gameSystem = character.attributes.all()[0].gameSystem
+  if isinstance(attribute, str) or isinstance(attribute, unicode):
+    attribute = CharacterAttributeDefinition.objects.get(gameSystem = gameSystem, name = attribute)
+
+  return '<span id="attr_%d">%s</span>' % (
+    attribute.id,
+    character.getAttribute(attribute),
+  )
+
--- teledusite/teledu/tests.py
+++ teledusite/teledu/tests.py
@@ -4,9 +4,11 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+from django.template import Context
 
 from django.test import TestCase
 from .models import GameSystem, CharacterAttributeDefinition, Character, CharacterAttribute
+from .templatetags.character import char_attr
 
 class WhenGettingCharacterAttributeValue(TestCase):
   def setUp(self):
@@ -33,3 +35,36 @@ class WhenGettingCharacterGameSystem(TestCase):
   def testThatCorrectGameSystemIsReturned(self):
     actual = self.char.gameSystem
     self.assertEqual(actual, self.gameSystem)
+
+class WhenEmbeddingCharacterAttribute(TestCase):
+  def setUp(self):
+    self.gameSystem = GameSystem.objects.create(name = 'something')
+    self.attrDefn = CharacterAttributeDefinition.objects.create(gameSystem = self.gameSystem, name = 'whatever')
+    self.char = Character.objects.create(name = 'whomever')
+    self.attributeValue = 'a testing value'
+    self.charAttr = CharacterAttribute.objects.create(character = self.char, attribute = self.attrDefn, value = self.attributeValue)
+    self.context = Context()
+    self.context['character'] = self.char
+
+  def assertCorrectAttributeElement(self, elementText):
+    self.assertEqual(elementText, '<span id="attr_%d">%s</span>' % (self.attrDefn.id, self.attributeValue))
+
+  def testThatAttributeElementIsReturnedUsingAttributeReference(self):
+    actual = char_attr(self.context, self.attrDefn)
+    self.assertCorrectAttributeElement(actual)
+
+  def testThatAttributeElementIsReturnedUsingAttributeName(self):
+    actual = char_attr(self.context, self.attrDefn.name)
+    self.assertCorrectAttributeElement(actual)
+
+  def testThatAttributeElementIsReturnedUsingAttributeNameAsUnicode(self):
+    actual = char_attr(self.context, unicode(self.attrDefn.name))
+    self.assertCorrectAttributeElement(actual)
+
+  def testThatAttributeElementIsReturnedUsingAttributeNameWhenManyGameSystemsHaveSameAttributeName(self):
+    gameSystem = GameSystem.objects.create(name = 'something else')
+    CharacterAttributeDefinition.objects.create(gameSystem = gameSystem, name = self.attrDefn.name)
+
+    actual = char_attr(self.context, self.attrDefn.name)
+    self.assertCorrectAttributeElement(actual)
+
