abdd06d346e6d1b3286e3b89d721369ed3696f5c
Tim Court <tctimmeh@gmail.com>
2012-10-16 20:27:09 -0600

    added acceptance test for editing concept-type character attributes using an html select

--- teledusite/teledu/tests/acceptance/testCharacter.py
+++ teledusite/teledu/tests/acceptance/testCharacter.py
@@ -1,3 +1,5 @@
+import unittest
+from selenium.webdriver.support.select import Select
 from teledu.models import CharacterAttribute
 from teleduLiveTestCase import TeleduLiveTestCase, setUpModule
 from selenium.webdriver.support.ui import WebDriverWait
@@ -56,9 +58,21 @@ class TestCharacterSheet(TeleduLiveTestCase):
     self.editAttributeAndWaitForChange(self.charAttrDefn, expected)
     self.assertCharacterAttributeHasValue(self.charAttr, expected)
 
-  def testEdittingAttributeWithDependentsUpdatesDependentAttributes(self):
+  def testEditingAttributeWithDependentsUpdatesDependentAttributes(self):
     expected = self.uniqStr()
     self.editAttributeAndWaitForChange(self.charAttrDefn, expected, changeDefinition = self.dependentCharAttrDefn)
     self.assertCharacterAttributeHasValue(self.dependentCharAttr, expected)
 
+  @unittest.expectedFailure
+  def testChoosingNewValueForConceptAttributeUpdatesCharacterInDatabase(self):
+    conceptInstance2 = self.createConceptInstance()
+
+    element = self.driver.find_element_by_id('attr_%d' % self.conceptCharAttrDefn.id)
+    element.click()
+    select = Select(element.find_element_by_tag_name('select'))
+    self.assertEqual(select.first_selected_option.text, self.conceptInstance.name)
+
+    select.select_by_visible_text(conceptInstance2.name)
+    WebDriverWait(self.driver, 5).until(lambda driver: self.elementHasText(element, conceptInstance2.name))
+    self.assertCharacterAttributeHasValue(self.conceptCharAttrDefn, conceptInstance2.id)
 
