c9c83d6ff3392551398a32bc2e27a1b1b49cefb1
Tim Court <tctimmeh@gmail.com>
2012-10-25 20:51:24 -0600

    moved getting attribute value tests to attribute suite from character attribute suite

--- teledusite/teledu/tests/models/character/whenGettingAttributeValue.py
+++ teledusite/teledu/tests/models/character/whenGettingAttributeValue.py
@@ -13,39 +13,3 @@ class WhenGettingCharacterAttributeValue(TeleduTestCase):
     actual = self.character.getAttributeValue(self.charAttr.name)
     self.assertEqual(actual, self.charAttrValue.value)
 
-  def testThatEmptyValueIsReturnedForConceptAttributeWithEmptyRawValue(self):
-    attribute = self.addAttributeToCharacter(type = 'concept', concept = self.concept)
-    actual = self.character.getAttributeValue(attribute)
-    self.assertEqual(actual, '')
-
-  def testThatConceptInstanceNameIsReturnedForConceptAttributeWithValidConceptId(self):
-    instance = self.createConceptInstance()
-    attribute = self.addAttributeToCharacter(type = 'concept', concept = self.concept, default = instance.id)
-    actual = self.character.getAttributeValue(attribute)
-    self.assertEqual(actual, instance.name)
-
-  def testThatListAttributesReturnListOfValues(self):
-    self.charAttr.list = True
-    self.charAttr.save()
-    attr2 = self.createAttributeValueForCharacter(self.charAttr)
-    expected = [self.charAttrValue.value, attr2.value]
-    expected.sort()
-    actual = self.character.getAttributeValue(self.charAttr)
-    actual.sort()
-    self.assertEqual(actual, expected)
-
-  def testThatListAttributesWithNoValuesReturnEmptyList(self):
-    attribute = self.createAttribute(list = True)
-    actual = self.character.getAttributeValue(attribute)
-    self.assertEqual(actual, [])
-
-  def testThatListValuesAreReturnedAsCorrectTypes(self):
-    attribute = self.createAttribute(type = 'integer', list = True)
-    attr1 = self.createAttributeValueForCharacter(attribute, initialValue=self.uniqInt())
-    attr2 = self.createAttributeValueForCharacter(attribute, initialValue=self.uniqInt())
-    expected = [attr1.value, attr2.value]
-    expected.sort()
-    actual = self.character.getAttributeValue(attribute)
-    actual.sort()
-    self.assertEquals(actual, expected)
-
